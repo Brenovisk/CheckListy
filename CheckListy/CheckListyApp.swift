7f5cb7520a06f23b9269413076daad4bac2f783b
Brenovisk <brenolucasdeos@gmail.com>
2024-07-02 10:08:59 -0300

    Initial Commit

---
+++ CheckListy/CheckListyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListyApp.swift
+//  CheckListy
+//
+//  Created by Breno Lucas on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckListyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
